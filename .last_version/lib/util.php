79a7b0b7da9668e2ae06ac32f0a40a42ee1cd841
Dokukin Vyacheslav Olegovich <toorrp4@gmail.com>
2024-04-14 00:08:24 +0300

    Исправления по классу Util

--- .last_version/lib/util.php
+++ .last_version/lib/util.php
@@ -1,21 +1,20 @@
 <?php
-namespace Itscript\Book;
+namespace Itscript\Question;
 
 use Bitrix\Main;
 use Bitrix\Main\Config\Option;
 
 IncludeModuleLangFile(__FILE__);
 
-class General
+class Util
 {
-    const MODULE_ID = "itscript.config";
+    const MODULE_ID = "itscript.question";
 
     /**
      * Function print var
      * @param $value
      */
-    public static function debug($value)
-    {
+    public static function debug($value) {
         echo "<br/><pre style='padding:10px; border:1px solid #DDD; background-color:#EEE; text-color:#000; font-family:Verdana; font-size:13px;'>";
 
         switch (gettype($value))
@@ -34,19 +33,14 @@ class General
         echo "</pre><br/>";
     }
 
-
-	public static function writeSysLog($auditTypeId, $itemId, $description, $severity = 'DEBUG', $moduleId = self::MODULE_ID)
-	{
-		//if(empty($auditTypeId) || !intval($itemId) || ITSERW_COWMS_USE_SYSLOG != 'Y')
-			//return false;
-
-		\CEventLog::Add(array(
+	public static function writeSysLog($auditTypeId, $itemId, $description, $severity = 'DEBUG') {
+		\CEventLog::Add([
 			"SEVERITY" => $severity,
 			"AUDIT_TYPE_ID" => $auditTypeId,
-			"MODULE_ID" => $moduleId,
+			"MODULE_ID" => self::MODULE_ID,
 			"ITEM_ID" => $itemId,
 			"DESCRIPTION" => $description,
-		));
+		]);
 	}
 
 }
